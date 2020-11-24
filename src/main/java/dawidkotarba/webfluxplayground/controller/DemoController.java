756d97e52375471919d11d9040e691d6f329f9d5
dawidkotarba <dawidkotarba>
2020-11-24 16:31:14 +0100

    Change producing to APPLICATION_STREAM_JSON_VALUE

--- src/main/java/dawidkotarba/webfluxplayground/controller/DemoController.java
+++ src/main/java/dawidkotarba/webfluxplayground/controller/DemoController.java
@@ -20,38 +20,38 @@ public class DemoController {
     private final Flux<Integer> hotPublisherFromCold = Flux.from(coldPublisher).cache();
 
     // Sequential retrieveal every second
-    @GetMapping(path = "/fluxIntegerSequenceNoDelay", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @GetMapping(path = "/fluxIntegerSequenceNoDelay", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
     public Flux<Integer> fluxIntegerSequenceNoDelay() {
         return Flux.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
     }
 
     // http://localhost:8080/fluxIntegerSequence
     // Sequential retrieveal every second
-    @GetMapping(path = "/fluxIntegerSequence", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @GetMapping(path = "/fluxIntegerSequence", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
     public Flux<Integer> fluxIntegerSequence() {
         return Flux.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).delayElements(Duration.ofSeconds(1));
     }
 
     // http://localhost:8080/coldPublisher
-    @GetMapping(path = "/coldPublisher", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @GetMapping(path = "/coldPublisher", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
     public Flux<Integer> coldPublisher() {
         return coldPublisher;
     }
 
     // http://localhost:8080/hotPublisher
-    @GetMapping(path = "/hotPublisher", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @GetMapping(path = "/hotPublisher", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
     public Flux<Integer> hotPublisher() {
         return hotPublisher;
     }
 
     // http://localhost:8080/hotPublisherFromCold
-    @GetMapping(path = "/hotPublisherFromCold", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @GetMapping(path = "/hotPublisherFromCold", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
     public Flux<Integer> hotPublisherFromCold() {
         return hotPublisherFromCold;
     }
 
     // http://localhost:8080/zipPublishers
-    @GetMapping(path = "/zipPublishers", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @GetMapping(path = "/zipPublishers", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
     public Flux<Integer> zipPublishers() {
         return hotPublisher.doOnNext(integer -> System.out.println("Publishing: " + integer))
                 .zipWith(coldPublisher, Integer::sum).doOnNext(integer -> System.out.println("Sum of publishers: " + integer));
